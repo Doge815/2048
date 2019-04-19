cabed1bcb4f065483a15fc1b061518d0ded12cc5
Doge815 <42283470+Doge815@users.noreply.github.com>
2019-04-19 12:45:15 +0200

    all in

---
+++ 2048/2048/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Block
+{
+    GameObject Box;
+    Point p;
+    public Point P { get => p; set { p = value; SetPoint(); } }
+
+    public Block(GameObject g, Point p)
+    {
+        this.p = p;
+        float w = g.GetComponent<RectTransform>().rect.width;
+        float rr = g.GetComponent<RectTransform>().rect.height;
+        GameObject h = GameObject.Find("Holder");
+        RectTransform r = h.GetComponent<RectTransform>();
+        Box = MonoBehaviour.Instantiate(g, h.transform);
+        SetPoint();
+    }
+
+    private void SetPoint()
+    {
+        RectTransform r = Box.GetComponent<RectTransform>();
+        RectTransform h = GameObject.Find("Holder").GetComponent<RectTransform>();
+        Vector3 v = new Vector3(h.rect.width / 3 * p.X + r.rect.width, h.rect.height / 3 * p.Y + r.rect.height, 0);
+        Box.GetComponent<RectTransform>().position = v;
+
+    }
+}
---
+++ 2048/2048/Assets/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Drawing;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public static int Size { get; set; } = 3;
+    public GameObject block;
+    public GameObject Block { get => block;}
+    private GameObject holder;
+    public GameObject Holder { get => holder; }
+    Block b;
+    
+    void Start()
+    {
+        holder = GameObject.Find("Holder");
+        b = new Block(block, new System.Drawing.Point(0, 0));
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            b.P = new Point(b.P.X, b.P.Y + 1);
+        }
+    }
+}
