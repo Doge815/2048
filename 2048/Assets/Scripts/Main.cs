b304278958d65f7ae3a972049694050d88b6e7db
blenderfreaky <blendcrafterxiv@gmail.com>
2019-04-30 18:11:05 +0200

    still doesnt work but its better (#1)
    * Update Main.cs
    
    * Did stuff
    
    made block into script
    
    * shit dont work
    
    * move back
    
    * make mege ez

--- 2048/Assets/Scripts/Main.cs
+++ 2048/Assets/Scripts/Main.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Drawing;
@@ -9,17 +9,21 @@ using TMPro;
 
 public class Main : MonoBehaviour
 {
-    public static int Size { get; set; } = 20;
-    private Block[][] Box { get; set; }
+    public static int Size { get; set; } = 4;
+    private Block[,] Box { get; set; }
 
-    public GameObject block;
-    public GameObject bg;
-    public GameObject Scoreboard;
+    public GameObject blockPrefab;
+    public GameObject backgroundPrefab;
+    public GameObject holder;
 
-    private GameObject holder;
-    public GameObject Holder { get => holder; }
-    private static float min;
-    public static float Min { get => min; private set => min = value; }
+    private GameObject backgroundInstance;
+    private GameObject Scoreboard;
+
+    public static float Min { get; private set; }
+    public static int Score { get; private set; } = 0;
+
+    enum Split { horizontal, vertical };
+    enum Direction { left, right };
 
     private static int score = 0;
     public static int Score { get => score; private set => score = value; }
@@ -28,36 +32,34 @@ public class Main : MonoBehaviour
     enum Direction { left, right };
     void Start()
     {
-        holder = GameObject.Find("Holder");
-        Min = new List<float> { holder.GetComponent<RectTransform>().rect.size.x, holder.GetComponent<RectTransform>().rect.size.y }.Min();
+        Min = Mathf.Min(holder.GetComponent<RectTransform>().rect.size.x, holder.GetComponent<RectTransform>().rect.size.y);
 
-        Box = new Block[Size][];
-        for (int i = 0; i < Size; i++) Box[i] = new Block[Size];
+        Box = new Block[Size,Size];
         #region background
-        GameObject g = Instantiate(bg, holder.transform);
-        g.GetComponent<RectTransform>().sizeDelta = new Vector2(Min, Min);
-        Scoreboard.GetComponent<RectTransform>().sizeDelta = new Vector2((holder.GetComponent<RectTransform>().rect.size.x - Min)/2, holder.GetComponent<RectTransform>().rect.size.y);
-        Scoreboard.GetComponent<RectTransform>().position = new Vector3((holder.GetComponent<RectTransform>().rect.size.x -Min) / 4, Min / 2);
+        backgroundInstance = Instantiate(backgroundPrefab, holder.transform);
+        backgroundInstance.GetComponent<RectTransform>().sizeDelta = new Vector2(Min, Min);
+
+        Scoreboard.GetComponent<RectTransform>().sizeDelta = new Vector2((holder.GetComponent<RectTransform>().rect.size.x - Min) / 2, holder.GetComponent<RectTransform>().rect.size.y);
+        Scoreboard.GetComponent<RectTransform>().position = new Vector3((holder.GetComponent<RectTransform>().rect.size.x - Min) / 4, Min / 2);
+
         #endregion
-        Box[0][0] = new Block(block, new Point(0, 0), 2);
-        Box[0][2] = new Block(block, new Point(2, 0), 2);
-        //InvokeRepeating("whynot", 0.5f, 0.1f);
+        Box[0,0] = new Block(blockPrefab, new Point(0, 0), 2);
+        Box[0,2] = new Block(blockPrefab, new Point(2, 0), 2); ;
     }
     void OnGUI()
     {
-        GameObject g = GameObject.Find("BackGround(Clone)");
-        Texture t = g.GetComponent<Image>().mainTexture;
+        Texture t = backgroundInstance.GetComponent<Image>().mainTexture;
         float r = Min / (Size * 6 + 1);
         UnityEngine.Color origin = GUI.color;
         GUI.color = new Color32(187, 173, 160, 255);
         for (int i = 0; i < Size + 1; i++)
         {
-            Rect a = new Rect(new Vector2(Holder.GetComponent<RectTransform>().rect.size.x / 2 - Min / 2 + i * 6 * r, Holder.GetComponent<RectTransform>().rect.size.y / 2 - Min / 2),new Vector2(r, Min));
+            Rect a = new Rect(new Vector2(holder.GetComponent<RectTransform>().rect.size.x / 2 - Min / 2 + i * 6 * r, holder.GetComponent<RectTransform>().rect.size.y / 2 - Min / 2),new Vector2(r, Min));
             GUI.DrawTexture(a, t);
         }
         for (int i = 0; i < Size + 1; i++)
         {
-            Rect a = new Rect(new Vector2(Holder.GetComponent<RectTransform>().rect.size.x / 2 - Min / 2, Holder.GetComponent<RectTransform>().rect.size.y / 2 - Min / 2 + i * 6 * r), new Vector2(Min, r));
+            Rect a = new Rect(new Vector2(holder.GetComponent<RectTransform>().rect.size.x / 2 - Min / 2, holder.GetComponent<RectTransform>().rect.size.y / 2 - Min / 2 + i * 6 * r), new Vector2(Min, r));
             GUI.DrawTexture(a, t);
         }
         GUI.color = origin;
@@ -88,29 +90,27 @@ public class Main : MonoBehaviour
             split = (int)Split.vertical;
             direction = (int)Direction.left;
         }
-        if (split == null) return;
-        move(split.Value, direction.Value);
+        if (split.HasValue) move(split.Value, direction.Value);
         #endregion
         
     }
     private void move(int split, int direction)
     {
         #region split array
-        Block[][] Temp = new Block[Size][];
-        for (int i = 0; i < Size; i++) Temp[i] = new Block[Size];
+        Block[,] Temp = new Block[Size, Size];
         for (int i = 0; i < Size; i++)
         {
             for (int u = 0; u < Size; u++)
             {
-                if (split == (int)Split.horizontal) Temp[i][u] = Box[i][u];
-                else Temp[i][u] = Box[u][i];
+                if (split == (int)Split.horizontal) Temp[i,u] = Box[i, u];
+                else Temp[i, u] = Box[u, i];
             }
         }
         #endregion
         #region move blocks
         for (int i = 0; i < Size; i++)
         {
-            List<Block> TempList = (from t in Temp[i] where t != null select t).ToList();
+            List<Block> TempList = (from u in Enumerable.Range(0, Size) where Temp[i, u] != null select Temp[i, u]).ToList();
             for (int u = 1; u < TempList.Count; u++)
             {
                 if (TempList[u].Value == TempList[u - 1].Value)
@@ -121,10 +121,19 @@ public class Main : MonoBehaviour
                     TempList.RemoveAt(u - 1);
                 }
             }
-            Temp[i] = new Block[Size];
+
             for (int u = 0; u < TempList.Count; u++)
             {
-                Temp[i][(direction == (int)Direction.right) ? (Size - u - 1) : (u)] = TempList[(direction == (int)Direction.right) ? (TempList.Count - u - 1) : (u)];
+                int newU = (direction == (int)Direction.right) ? (Size - u - 1) : u;
+
+                Block block;
+                if (u < TempList.Count) {
+                    block = TempList[(direction == (int)Direction.right) ? (TempList.Count - u - 1) : u];
+                    block.P = new Point(i, newU);
+                }
+                else block = null;
+
+                Temp[i, newU] = block;
             }
             //if (direction == (int)Direction.right) Array.Reverse(Temp[i]);
         }
@@ -132,23 +141,10 @@ public class Main : MonoBehaviour
         #region rebuild array
         for (int i = 0; i < Size; i++)
         {
-            Box[i] = new Block[Size];
-        }
-        for (int i = 0; i < Size; i++)
-        {
             for (int u = 0; u < Size; u++)
             {
-                if (split == (int)Split.horizontal) Box[i][u] = Temp[i][u];
-                else Box[i][u] = Temp[u][i];
-            }
-        }
-        #endregion
-        #region set points
-        for (int i = 0; i < Size; i++)
-        {
-            for (int u = 0; u < Size; u++)
-            {
-                if (Box[i][u] != null) Box[i][u].P = new Point(u, i);
+                if (split == (int)Split.horizontal) Box[i,u] = Temp[i,u];
+                else Box[i,u] = Temp[u,i];
             }
         }
         #endregion
@@ -158,13 +154,12 @@ public class Main : MonoBehaviour
         {
             for (int u = 0; u < Size; u++)
             {
-                if (Box[i][u] == null) places.Add(new Point(i, u));
+                if (Box[i,u] == null) places.Add(new Point(i, u));
             }
         }
         Point TheChosenOne = places[UnityEngine.Random.Range(0, places.Count)];
-        Box[TheChosenOne.X][TheChosenOne.Y] = new Block(block, new Point(TheChosenOne.Y, TheChosenOne.X), 2);
+        Box[TheChosenOne.X,TheChosenOne.Y] = new Block(blockPrefab, new Point(TheChosenOne.Y, TheChosenOne.X), 2);
         #endregion
-        GC.Collect();
     }
 
 
